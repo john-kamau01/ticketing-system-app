4abd8891d3f3c6d69b34d097b8812f4a2365e1f0
John Kamau <johnwaweru092@gmail.com>
2024-07-11 19:12:51 +0300

    Added error handling middleware

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,22 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const { logger } = require("./middleware/logger.js");
+const { errorHandler } = require("./middleware/errorHandler.js");
 const PORT = process.env.PORT || 3500;
 
-// Use Static files
-app.use("/", express.static(path.join(__dirname, "/public")));
+app.use(logger);
 
-// routes
+app.use(express.json());
+app.use("/", express.static(path.join(__dirname, "public")));
+
+// ROUTES
 app.use("/", require("./routes/root.js"));
 
 // NOT FOUND
 app.all("*", (req, res) => {
-    res.status(404)
+    res.status(404);
+    
     if(req.accepts("html")){
         res.sendFile(path.join(__dirname, "views", "404.html"));
     } else if(req.accepts("json")){
@@ -19,7 +24,9 @@ app.all("*", (req, res) => {
     } else {
         res.type("txt").send("404 Not Found");
     }
-})
+});
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
     console.log(`Server runnning on http://localhost:${PORT}...`);
